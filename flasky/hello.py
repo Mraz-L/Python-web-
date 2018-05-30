350a5a4dd406f2c723d7432253fa61780fbad4d7
Marz-L <940049051@qq.com>
2018-05-30 10:19:33 +0800

    commit

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,19 +1,29 @@
 # -*- coding: UTF-8 -*-
 #初始化
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_script import Manager
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
-#路由和视图函数
+#路由和视图函数,渲染
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
-#增加动态路由
+#增加动态路由,渲染
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, %s!</h1>' % name
+	return render_template('user.html', name=name)
+
+#自定义错误页面
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500
 
 #URL中动态参数id对应的用户不存在，返回状态码404
 @app.route('/user/<id>')
